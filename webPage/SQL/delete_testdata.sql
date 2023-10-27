8c01a0d1521d9be6c5d00024476b7c2b6c15d39e
kimsh0710 <kimsh6622_@naver.com>
2023-10-27 11:36:13 +0900

    delete

--- webPage/SQL/delete_testdata.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM tb_user;
-
-set sql_safe_updates=0;
-DELETE FROM tb_user WHERE Customer_ID = '1' and Membership = '1';
--- webPage/Telco_defence/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-import joblib
-
-class TelcoDefenceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Telco_defence'
-    mlmodels = joblib.load("./Predict_model/SGD_model.pkl")  # Mechine Learning Model Pre-load
